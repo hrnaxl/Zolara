3a89d9fc950a31c5aa8e1c7f90ddef50a1ff3ad1
Zolara Fix <fix@zolara.com>
2026-03-10 20:54:56 +0000

    fix: remove duplicate SettingsProvider in main.tsx causing blank page after login

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,5 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-import { SettingsProvider } from "./context/SettingsContext.tsx";
 
-createRoot(document.getElementById("root")!).render(
-  <SettingsProvider>
-    <App />
-  </SettingsProvider>
-);
+createRoot(document.getElementById("root")!).render(<App />);
